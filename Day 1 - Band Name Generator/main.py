75ccec40dd740fe87cb709700d71a71397253760
Grayfox <gabrieldutradev@outlook.com>
2023-10-05 23:47:12 -0300

    Update main.py

--- Day 1 - Band Name Generator/main.py
+++ Day 1 - Band Name Generator/main.py
@@ -1,5 +1,5 @@
-print("Hello, padawan! This is the Band Name Generator!\n")
-city = input("What city do you grew up?\n")
-pet = input("What is your pet's name?\n")
-print("Your band's name is " + city + " " + pet)
+print("Welcome to the band name generator.\n")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
 
