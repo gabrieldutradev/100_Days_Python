f79395b6caf802c125d5d201622f0a17af19198d
Grayfox <gabrieldutradev@outlook.com>
2023-10-05 23:38:17 -0300

    Primeiro commit de Python!

---
+++ Day 1 - Band Name Generator/main.py
@@ -0,0 +1,5 @@
+print("Hello, padawan! This is the Band Name Generator!\n")
+city = input("What city do you grew up?\n")
+pet = input("What is your pet's name?\n")
+print("Your band's name is " + city + " " + pet)
+
