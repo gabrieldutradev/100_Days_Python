b686ed032c5f6bf3f6562663f1e57aac5204b1d5
Grayfox <gabrieldutradev@outlook.com>
2023-10-08 04:01:20 -0300

    final

---
+++ Day 2 - Tip Calculator/main.py
@@ -0,0 +1,17 @@
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill? $"))
+print(type(bill))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+print(type(tip))
+people = int(input("How many people to split the bill? "))
+print(type(people))
+
+bill_with_tip = bill + (bill * (tip/100))
+bill_each_person = round((bill_with_tip / people), 2)
+print(bill_each_person)
+bill_each_person = "{:.2f}".format((bill_with_tip/people))
+print(bill_each_person)
+bill_each_person = "{:.3f}".format((bill_with_tip/people))
+print(bill_each_person)
+
+print(f"Each person should pay: ${bill_each_person}")
