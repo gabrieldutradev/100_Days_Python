6546dcec01304e71c256c970711b522cee436e15
Grayfox <gabrieldutradev@outlook.com>
2023-10-09 19:20:32 -0300

    Alteração rollercoaster

--- Day 3 - Treasure Island/rollercoaster.py
+++ Day 3 - Treasure Island/rollercoaster.py
@@ -11,6 +11,8 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Younth tickets are $7.")
+    elif age >=45 and age <=55:
+        print("Everything is going to be okay. Have a free ride on us!")
     else:
         bill = 12
         print("Adults tickets are $12.")
