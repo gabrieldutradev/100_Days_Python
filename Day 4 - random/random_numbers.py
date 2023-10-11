af9b3a6d48096d0105d072cd3d1d0f51c6c71719
Grayfox <gabrieldutradev@outlook.com>
2023-10-10 22:40:37 -0300

    only for today

---
+++ Day 4 - random/random_numbers.py
@@ -0,0 +1,17 @@
+import random
+import my_module
+
+ranndom_integer = random.randint(1,10)
+print(ranndom_integer)
+
+# print(my_module.pi)
+
+# Float random
+random_float = random.random()
+print(random_float)
+
+random_float = random.random() * 5
+print(random_float)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}!")
